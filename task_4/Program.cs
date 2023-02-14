f3812134f947de51db0b46a5aa4e70c3a356336f
Elim Yunusov <elim.yunusov1@gmail.com>
2023-02-15 02:05:20 +0400

    4th task from 3.1 seminar

---
+++ task_4/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+System.Console.WriteLine("Enter number: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int numbers = 2;
+if (N > 1)
+{
+
+    while (numbers <= N)
+    {
+        System.Console.Write(numbers + " ");
+        numbers += 2;
+    }
+}
+else
+{
+    System.Console.Write("No even numbers from 1 to N");
+}

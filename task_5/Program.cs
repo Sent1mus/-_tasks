516eba7793e173c8b08ed948ca19c454bbcb00be
Elim Yunusov <elim.yunusov1@gmail.com>
2023-02-17 00:58:50 +0400

    1st task from 3.2 seminar

---
+++ task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого
+// числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.Clear();
+System.Console.WriteLine("Enter three-digit number: ");
+int number = int.Parse(Console.ReadLine());
+int result = 0;
+if (Math.Abs(number) > 99 && Math.Abs(number) < 1000)
+{
+    result = Math.Abs((number / 10) % 10);
+    System.Console.WriteLine($"second digit is: {result}");
+}
+else
+{
+    System.Console.WriteLine("Number is not three-digit");
+}

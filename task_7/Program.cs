fc336496063414496e7bc7a2c5b2441b0b3d6e04
Elim Yunusov <elim.yunusov1@gmail.com>
2023-02-17 02:03:19 +0400

    3rd task from 3.2 seminar

---
+++ task_7/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход
+// цифру, обозначающую день недели, и проверяет, является ли
+// этот день выходным.
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.Clear();
+System.Console.WriteLine("Enter number of the day: ");
+int day = Convert.ToInt32(Console.ReadLine());
+if (day < 1 || day > 7)
+{
+    System.Console.WriteLine($"Nonexisting day");
+}
+else if (day > 0 && day < 6)
+{
+    System.Console.WriteLine("Work day");
+}
+else
+{
+    switch (day)
+    {
+        case 6:
+            System.Console.WriteLine("Saturday - it's a holiday");
+            break;
+        case 7:
+            System.Console.WriteLine("Sunday - it's a holiday");
+            break;
+    }
+}

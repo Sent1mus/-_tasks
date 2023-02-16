93ca344ea073f89e2291cc5bc71070934b4d1893
Elim Yunusov <elim.yunusov1@gmail.com>
2023-02-17 01:42:20 +0400

    2nd task from 3.2 seminar

---
+++ task_6/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью
+// цифру заданного числа или сообщает,
+// что третьей цифры нет.
+
+Console.Clear();
+System.Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+while (number > 1000)
+{
+    number = number / 10;
+}
+if (number < 100)
+{
+    System.Console.WriteLine("No third digit");
+}
+else
+{
+    System.Console.WriteLine($"Third digit is {number % 10}");
+}

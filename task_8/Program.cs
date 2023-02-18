0665c758d722359e5cfe4a4578027886b604a50f
Elim Yunusov <elim.yunusov1@gmail.com>
2023-02-18 19:05:34 +0400

    1st task from 3.3 seminar

---
+++ task_8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+System.Console.WriteLine("Enter 5 digit number: ");
+string number = Console.ReadLine();
+int length = number.Length;
+if (length==5)
+{
+    if (number[0]==number[4] && number[1]==number[3])
+    {
+        System.Console.WriteLine("Number is palindrom");
+    }
+    else
+    {
+        System.Console.WriteLine("Number is NOT palindrom");
+    }
+}
+else
+{
+    System.Console.WriteLine("Number is not 5 digit");
+}

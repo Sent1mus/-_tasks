2905a75032aa77bc0eb49e64d5c8cd263fd4a785
Elim Yunusov <elim.yunusov1@gmail.com>
2023-02-21 01:50:21 +0400

    2nd task from 3.4 seminar

---
+++ task_12/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Clear();
+System.Console.WriteLine("Enter number: ");
+string? number = Console.ReadLine();
+int result = 0;
+if (number[0] != '-')                                           // я хз, но как-то я пришел к такому костылю на проверку отриц числа,
+{                                                               // тк иначе просто не хотел считать отриц число
+    for (int i = 0; i < number.Length; i++)
+    {
+        int temp = Convert.ToInt32(Convert.ToString(number[i]));
+        result = result + temp;
+    }
+}
+else
+    for (int i = 1; i < number.Length; i++)
+    {
+        int temp = Convert.ToInt32(Convert.ToString(number[i]));
+        result = result + temp;
+    }
+System.Console.WriteLine(result);

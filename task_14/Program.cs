81efcf7201905d5fa08b41e61cfe78550539b72e
Elim Yunusov <elim.yunusov1@gmail.com>
2023-02-22 03:36:35 +0400

    1st task from 3.5 seminar

---
+++ task_14/Program.cs
@@ -0,0 +1,52 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+int Promt(string message)
+{
+    System.Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+    int[] array = new int[Length];
+    Random random = new Random();
+    for (int i = 0; i < Length; i++)
+    {
+        array[i] = random.Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+void CountEven(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] % 2 == 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine("");
+    System.Console.WriteLine($"Quantity of even numbers = {count}");
+}
+
+void PrintArray(int[] array)
+{
+    System.Console.Write("[");
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        System.Console.Write($"{array[i]}, ");
+    }
+    System.Console.Write($"{array[array.Length - 1]}");
+    System.Console.Write("]");
+}
+
+int lenght = Promt("Enter array lenght: ");
+int minValue = 100;
+int maxValue = 999;
+int[] Array = GenerateArray(lenght, minValue, maxValue);
+PrintArray(Array);
+CountEven(Array);

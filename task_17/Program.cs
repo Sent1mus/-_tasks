724cd457e9f6c03869cd1d5b52100397e7af8efa
Elim Yunusov <elim.yunusov1@gmail.com>
2023-02-25 01:53:14 +0400

    1st task from 3.4 seminar

---
+++ task_17/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+// 0, 7, 8, -2, -2 -> 2
+// -1, -7, 567, 89, 223-> 3
+
+Console.Clear();
+int[] InputArray(string message)
+{
+    System.Console.WriteLine(message);
+    var array = Array.ConvertAll(Console.ReadLine().Split(), Convert.ToInt32);
+    return array;
+}
+
+void CountPositive(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    System.Console.WriteLine($"Positive numbers: {count}");
+}
+
+int[] MyArray = InputArray("Enter array: ");
+CountPositive(MyArray);
